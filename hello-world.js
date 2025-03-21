0fe0133cceda453162f37a905179f602ce45084f
knucxd <alessandro.cnto@gmail.com>
2025-03-20 22:51:43 -0300

    Adicionando a função de limpar o campo de texto quando o usuário teclar Esc

--- hello-world.js
+++ hello-world.js
@@ -1,11 +1,12 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
 
-nameInput.onkeyup = (event) => {
-    if (nameInput.value === '') {
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        nameInput.value = '';
         messageP.innerText = '';
     }
-}
+});
 
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
